a63fdc7a9cc90998d587431d4e25b28e8b6f613a
szabolcsragaly <119423607+szabolcsragaly@users.noreply.github.com>
2023-12-21 04:22:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
 
 """
 # Welcome to Streamlit!
